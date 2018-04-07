8729fd567528daee5be03966819045c39e27fb77
Ryert <Ryert@users.noreply.github.com>
2018-04-06 21:56:10 -0400

    Optimized Classes
    Commented and fixed bugs

--- src/main/java/me/ryert/commands/RefreshData.java
+++ src/main/java/me/ryert/commands/RefreshData.java
@@ -1,35 +1,35 @@
 package me.ryert.commands;
 
-import me.ryert.player.data.Inventory;
 import me.ryert.player.data.Player;
 import me.ryert.utils.DatabaseManager;
 import me.ryert.utils.GuildManagement;
 import net.dv8tion.jda.core.entities.Member;
 import net.dv8tion.jda.core.entities.MessageChannel;
 
-import java.util.ArrayList;
-
 /**
  * Admin Command
  * Checks for any users who aren't set up.
+ *
  * @author Ryert
  */
 public class RefreshData {
 
+    //Method that gets called when command is run
     public static void run(MessageChannel channel) {
         channel.sendMessage("Scanning the player base...").queue();
         channel.sendMessage("Found " + scan() + " unregistered users.").queue();
     }
 
+    //Scans the player base looking for unregistered users
     public static int scan() {
         int badUser = 0;
         for (Member member : GuildManagement.CONTROLLER.getGuild().getMembers()) {
-            if (!DatabaseManager.isPresent(member.getUser().getId()))
+            if (!DatabaseManager.isPresent(member.getUser().getId())) {
                 DatabaseManager.add(member.getUser().getId(), new Player());
-            if (member.getRoles().size() == 0) {
-                GuildManagement.addRole(member, "Arrival");
                 badUser++;
             }
+            if (member.getRoles().size() == 0)
+                GuildManagement.addRole(member, "Arrival");
         }
         return badUser;
     }
--- src/main/java/me/ryert/player/data/Stats.java
+++ src/main/java/me/ryert/player/data/Stats.java
@@ -2,18 +2,12 @@ package me.ryert.player.data;
 
 import java.io.Serializable;
 
+/**
+ * This class holds all the stats a player has.
+ * @author Ryert
+ */
 public class Stats implements Serializable {
-    private int intelligence;
 
     public Stats() {
-        this.intelligence = 0;
-    }
-
-    public int getIntelligence() {
-        return intelligence;
-    }
-
-    public void setIntelligence(int intelligence) {
-        this.intelligence = intelligence;
     }
 }
